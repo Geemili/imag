9b11e5a0a21af95aabbdfd530193c2218338683d
Matthias Beyer <mail@beyermatthias.de>
2016-07-15 15:07:57 +0200

    Add function to find all imag executables in $PATH

--- bin/src/imag.bashcomp.sh
+++ bin/src/imag.bashcomp.sh
@@ -1,3 +1,19 @@
+function __imag_executables() {
+    echo -n "$PATH" | \
+    xargs \
+        -d: \
+        -I{} \
+        -r \
+        -- find \
+            -L {} \
+            -maxdepth 1 \
+            -mindepth 1 \
+            -type f \
+            -executable \
+            -printf '%P\n' 2>/dev/null | \
+    sort -u | grep "^imag"
+}
+
 _imag() {
     local cur prev opts
     COMPREPLY=()
